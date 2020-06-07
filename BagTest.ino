7767d43ceea79123a6f0101f66bc70769784242d
Greg Berardi <greg@berardi.us>
2020-06-06 21:04:12 -0500

    Changed serial speed back to 115200 also added some info to README.md

--- BagTest.ino
+++ BagTest.ino
@@ -73,7 +73,7 @@ void setup()
   lcd.print("Ambu Bag Fixture!");
 	// This delay is enough time to program if there are issues with the code/programming
   delay(3000);
-  Serial.begin(9600);
+  Serial.begin(115200);
   while(!Serial);    // time to get serial running
 
   //Init BME280 sensor
@@ -107,7 +107,7 @@ void loop()
   markLoopStart();
 
   float ch1Val = fs6122_readPressure_SmlpM();
-  float ch2Val = bme.readTemperature();
+  float ch2Val = bme.readPressure();
   float ch3Val = cpuLoad;
   newPosition = myEnc.read();
   openSwState = digitalRead(OPEN_SW);
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # BagTest
 
 This is written and tested on the Arduino Mega.
+
+Serial Port Info
+
+stty -F /dev/ttyACM0 115200
+cu -l /dev/ttyACM0 -s 115200
+
+ ~/arduino-1.8.12/arduino --upload BagTest.ino
+
